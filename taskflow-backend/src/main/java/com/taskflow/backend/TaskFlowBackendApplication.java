ca31d7732c2b633c3f6561d7e13367313369e2c1
iomarin88 <137875750+iomarin88@users.noreply.github.com>
2024-12-27 15:52:21 +0200

    Adaugare logica de conectare la baza de date

--- taskflow-backend/src/main/java/com/taskflow/backend/TaskFlowBackendApplication.java
+++ taskflow-backend/src/main/java/com/taskflow/backend/TaskFlowBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskFlowBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TaskFlowBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TaskFlowBackendApplication.class, args);
+    }
 }
